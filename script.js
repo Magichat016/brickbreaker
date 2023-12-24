2bac9799290bd2e7c5f722e8efcf0aab87616368
Magic hat <154031802+Magichat016@users.noreply.github.com>
2023-12-25 00:12:50 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
+
+let ball_x = 20; 
+let ball_y = 20;
+let ball_radius = 10;
+
+
+
+
+function balldraw(){
+    ctx.beginPath(); // パスの初期化
+    ctx.arc(ball_x, ball_y, ball_radius, 0, 2 * Math.PI); // (100, 50)の位置に半径30pxの円
+    ctx.fillStyle = 'red';
+    ctx.fill(); 
+    ctx.closePath(); // パスを閉じる
+    
+}
+
+function Alldraw(){
+    balldraw();
+}
+
